ce8a1751bde3d79788312cd22771a728de9c8d97
mashoodvm <mashoodvm19mashood786@gmail.com>
2018-02-28 12:55:21 +0530

    my first commit

---
+++ aswathy.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int sum=0;
+float avg,d;
+int x[10],n;
+printf("enter the no of elements:");
+scanf("%d",&n);
+printf("enter the elemnts:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&x[i]);
+sum=sum+x[i];
+}
+avg=sum/n;
+printf("sum is =%d"c 
+
